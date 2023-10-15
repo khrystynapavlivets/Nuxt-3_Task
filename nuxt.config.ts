43241d0c6708174f37dbe5fd9631cec5a25aa548
khrystynapavlivets <khristina.pavlivets@gmail.com>
2023-10-15 23:46:07 +0300

    added .nojekyll

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+export default {
+  devtools: { enabled: true },
+  app: {
+    baseURL: '/Nuxt-3_Task/', // baseURL: '/<repository>/'
+    buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
+  },
+  experimental: {
+    payloadExtraction: true
+  }
+}
 
 
